7bd8444977a35fb43ab2c4e354e6d1d721746719
MATUTINO <MATUTINO@DESKTOP-6UV0DLI>
2024-06-03 11:49:45 -0500

    EGv0004 CambioDAO

---
+++ src/com/distribuida/dao/ClienetDAOImpl.java
@@ -0,0 +1,43 @@
+package com.distribuida.dao;
+
+import java.util.List;
+
+import com.distribuida.entities.Cliente;
+
+public class ClienetDAOImpl implements ClienteDAO {
+
+	public ClienetDAOImpl() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente findOne(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void add(Cliente cliente) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void up(Cliente cliente) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void del(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

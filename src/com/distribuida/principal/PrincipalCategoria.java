f2d611353e942ddd692508f47e94d34c13cf247a
MATUTINO <MATUTINO@DESKTOP-6UV0DLI>
2024-06-05 11:30:37 -0500

    EGv0009 Se modifico la clase categoria y la clase principal categoria

--- src/com/distribuida/principal/PrincipalCategoria.java
+++ src/com/distribuida/principal/PrincipalCategoria.java
@@ -1,8 +1,12 @@
 package com.distribuida.principal;
 
+import java.util.List;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.distribuida.dao.CategoriaDAO;
+import com.distribuida.entities.Categoria;
+
 
 
 
@@ -11,7 +15,15 @@ public class PrincipalCategoria {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+		
 		CategoriaDAO categoriaDAO= context.getBean("categoriaDAOImpl", CategoriaDAO.class);
+		 List<Categoria> categoria = categoriaDAO.findAll();
+		 
+		    categoria.forEach(item -> {
+		    	
+		    	System.out.println(item.toString());
+		    });
+			context.close();
 
 	}
 
